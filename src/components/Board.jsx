ce053a930d438095da34d8ae89b9162de546ad87
Razzer101 <Rasmus@anders-net.dk>
2025-11-04 08:54:14 +0100

    TicTacToeBegun

---
+++ src/components/Board.jsx
@@ -0,0 +1,17 @@
+import Tile from "./Tile";
+
+export default function Board(){
+    return(
+        <div>
+            <Tile/>
+            <Tile/>
+            <Tile/>
+            <Tile/>
+            <Tile/>
+            <Tile/>
+            <Tile/>
+            <Tile/>
+            <Tile/>
+        </div>
+    )
+}
